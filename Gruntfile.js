be4a7084478febb348c4b87e17a35aaec7db311c
Kelvin Wong <kwong691@gmail.com>
2014-06-14 20:16:13 +0000

    Add Grunt to uglify js

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	concat: {
+	    options: {
+		separator: ';'
+	    },
+	    dist: {
+		src: ['public/js/*.js'],
+		dest: 'public/dist/<%= pkg.name %>.js'
+	    }
+	},
+	uglify: {
+	    options: {
+		banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+	    },
+	    dist: {
+		files: {
+		    'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+		}
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', [
+	'concat',
+	'uglify'
+    ]);
+};
